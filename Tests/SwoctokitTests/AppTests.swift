e0c4596cfd2b483493328c574a7731336da8227b
Franz Busch <privat@franz-busch.de>
2018-08-13 11:44:57 +0200

    remove token

--- Tests/SwoctokitTests/AppTests.swift
+++ Tests/SwoctokitTests/AppTests.swift
@@ -4,9 +4,6 @@ import Swoctokit
 
 final class AppTests: XCTestCase {
     func testNothing() throws {
-        let swoctokit = try Swoctokit(token: "bae5d647757d28a904080b28e0936ea19bd898b7")
-        let response = swoctokit.repository.createRepository(organization: "e-Sixt", name: "Test", description: "Test", isPrivate: true)
-        try response.wait()
     }
 
     static let allTests = [
